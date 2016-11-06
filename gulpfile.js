a548d97f961a2a22aa6516e3bc6a0bf8f530be16
Natalya Baganova <natalyabaganova@192.168.1.44>
2016-11-06 19:46:07 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,89 @@
+'use strict';
+
+var gulp =require('gulp');
+var sass = require('gulp-sass');
+var jade = require('gulp-jade');
+var runSequence = require('run-sequence');
+var concat = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer')
+
+
+
+gulp.task('sass', function () {
+    gulp.src('./sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+
+    gulp.src('./sass/templates/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/templates'));
+
+});
+
+gulp.task('jade', function() {
+    return gulp.src('./index.jade')
+        .pipe(jade()) // pip to jade plugin
+        .pipe(gulp.dest('./')); // tell gulp our output folder
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/blocks/*.scss', ['concat:scss']);
+    gulp.watch('./sass/components/*.scss', ['concat:scss']);
+    gulp.watch('./sass/layout/*.scss', ['concat:scss']);
+    gulp.watch('./sass/vendor/**/*.scss', ['concat:scss']);
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('jade:watch', function () {
+    gulp.watch('./**/*.jade', ['jade']);
+});
+
+gulp.task('default', function () {
+
+});
+
+
+gulp.task('concat:scss', function() {
+     gulp.src(['./sass/blocks/**/*.scss'])
+        .pipe(concat('_blocks.scss'))
+        .pipe(gulp.dest('./sass/'));
+
+    gulp.src(['./sass/components/**/*.scss'])
+        .pipe(concat('_components.scss'))
+        .pipe(gulp.dest('./sass/'));
+
+    gulp.src(['./sass/layout/**/*.scss'])
+        .pipe(concat('_layout.scss'))
+        .pipe(gulp.dest('./sass/'));
+
+    gulp.src(['./sass/vendor/**/*.scss'])
+        .pipe(concat('_vendor.scss'))
+        .pipe(gulp.dest('./sass/'));
+});
+
+gulp.task('prefix', function () {
+    return gulp.src('./css/global.css')
+        .pipe(autoprefixer({
+            browsers: ['last 16 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('prefix:watch', function (){
+    gulp.watch('./css/global.css', ['prefix']);
+});
+
+gulp.task('default', function(callback) {
+    runSequence(
+        'concat:scss',
+        'sass',
+        'jade',
+        'sass:watch',
+        'jade:watch',
+        'prefix:watch',
+
+
+        callback);
+});
+
---
+++ js/global.js
@@ -0,0 +1,198 @@
+$(function () {
+    $(document).foundation();
+
+    $('.certificates__carousel, .our-clients__carousel').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
+    $('.success-story__carousel').slick({
+        infinite: true,
+        dots: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
+
+    $('.top-slider__carousel').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
+
+//equalheight - одинаковая высота колонок
+//Пример списка элементов:
+//var eqElement = ".cat_container > div, .home_news > div"
+    var eqElement = ""
+    $(window).load(function () {
+        equalheight(eqElement);
+    }).resize(function () {
+        equalheight(eqElement);
+    });
+
+    /** swipebox*/
+
+    $('.swipebox').swipebox({
+        useCSS: true, // false will force the use of jQuery for animations
+        useSVG: true, // false to force the use of png for buttons
+        initialIndexOnArray: 0, // which image index to init when a array is passed
+        hideCloseButtonOnMobile: false, // true will hide the close button on mobile devices
+        removeBarsOnMobile: true, // false will show top bar on mobile devices
+        hideBarsDelay: 3000, // delay before hiding bars on desktop
+        videoMaxWidth: 1140, // videos max width
+        beforeOpen: function () {
+        }, // called before opening
+        afterOpen: null, // called after opening
+        afterClose: function () {
+        }, // called after closing
+        loopAtEnd: false // true will return to the first image after the last image is reached
+    });
+
+
+/** block-video */
+
+    $('.button-play, .loupe').on('click', function () {
+        $(this).siblings('.swipebox').click();
+    })
+
+
+    function backImg() {
+        $(window).load(function () {
+            var itemHeight = $('.block-video__item').height();
+            var itemWidth = $('.block-video__item').width();
+            var itemBg = $('.block-video__item-bg');
+
+            itemBg.css({
+                'width': itemWidth,
+                'height': itemHeight
+        });
+
+        })
+
+    }
+
+    backImg();
+    $(window).resize(backImg());
+
+/** block-video END */
+
+/** block-expirience*/
+
+$(window).load(function () {
+    $('.one-height').each(function () {
+        var generalHeight = $(this).find('.g-height').height();
+
+        $(this).find('.s-height').outerHeight(generalHeight);
+        });
+    });
+})
+
+/** block-expirience END*/
+
+/** footer-main-menu*/
+var footerMenu = $('.footer-main-menu__item');
+
+footerMenu.on('mouseover', function () {
+    $(this).addClass('active');
+})
+
+footerMenu.on('mouseout', function () {
+    $(this).removeClass('active');
+})
+/** footer-main-menu END*/
+
+
+
+
