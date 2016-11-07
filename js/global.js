facf275049f369227b1608ce10878144acdccbfb
natulka85 <natulka85@gmail.com>
2016-11-07 23:50:12 +0300

    7-10-16-6

--- js/global.js
+++ js/global.js
@@ -80,14 +80,14 @@ $(function () {
         dots: true,
         speed: 300,
         slidesToShow: 5,
-        slidesToScroll: 1,
+        slidesToScroll: 5,
         arrows: true,
         responsive: [
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 5,
-                    slidesToScroll: 1,
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
                     infinite: true,
                     dots: true
                 }
